e88aa137fae13f5a882152afaa17a5c7ce6d868a
i-Riyaj <riyajriyaj452@gmail.com>
2024-05-10 07:38:18 +0530

    uploading popUp

---
+++ PopUp/script.js
@@ -0,0 +1,9 @@
+const popUp = document.querySelector('#popUp');
+
+const openPopUp = () => {
+    popUp.style.visibility = 'visible';
+};
+
+const closePopUp = () => {
+    popUp.style.visibility = 'hidden';
+}
